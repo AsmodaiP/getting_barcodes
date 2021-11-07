ff1993e21e172372e621f1b58095cc768b03082e
Asmodai <asmoday.p@gmail.com>
2021-11-07 14:30:30 +0300

    Add info about barcodes and def for editing blank pdf

--- testing.py
+++ testing.py
@@ -1,12 +1,18 @@
 import datetime
+import json
 import pytz
 import requests
 import codecs
 import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfFileMerger
+from requests.api import head
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.units import mm, inch
 
-
+pdfmetrics.registerFont(TTFont('FreeSans', 'fonts/FreeSans.ttf'))
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
@@ -29,7 +35,7 @@ headers = {
 def create_pdf_stickers_by_ids(ids):
     url_for_getting_stikers = 'https://suppliers-api.wildberries.ru/api/v2/orders/stickers/pdf'
     data_for_pdf = ''
-    pdfs=[]
+    pdfs = []
     for id in ids:
         json_orders_id = {
             "orderIds": [int(id)]
@@ -50,7 +56,6 @@ def create_pdf_stickers_by_ids(ids):
         merger.append(pdf)
     merger.write("result2.pdf")
     merger.close()
-
 
     # file_data = bytes(data_for_pdf, 'utf-8')
 
@@ -95,27 +100,43 @@ def get_orders_ids(orders):
         ids.append(int(order['orderId']))
     return ids
 
-def get_barcodes_and_ids(orders):
+
+def get_barcodes_with_orders_and_chartId(orders):
     barcodes_and_ids = {}
     for order in orders:
         barcode = order['barcode']
         id = order['orderId']
-        if barcode not in  barcodes_and_ids.keys(): 
-            barcodes_and_ids[barcode] = [id]
+        chrt_id = order['chrtId']
+        if barcode not in barcodes_and_ids.keys():
+            barcodes_and_ids[barcode] = {'orders': [id], 'chrtId':chrt_id}
         else:
-            barcodes_and_ids[barcode] += [id]
+            barcodes_and_ids[barcode]['orders'] += [id]
     return barcodes_and_ids
 
+def edit_blank_pdf(barkode_info):
+    canvas = Canvas('pdf/blank.pdf',pagesize=(1.6*inch,  1.2*inch))
+    font_size = 8
+    canvas.setFont('FreeSans', font_size)
+    n=0
+    slice=27
+    while len(barkode_info['name'])>0:
+        canvas.drawString(2, 75-n*font_size, barkode_info['name'][:slice], charSpace=0)
+        barkode_info['name'] = barkode_info['name'][slice:]
+        print(barkode_info['name'])
+        n+= 1
+    canvas.drawString(2,75-font_size*n, barkode_info['article'])
+    n += 1
+    canvas.drawString(2,75-font_size*n, barkode_info['chrtId'])
+    canvas.save()   
 
 
-
-def create_pdf_stickers_by_barcodes_and_ids(barcodes_and_ids):
+def create_pdf_stickers_by_barcodes(barcodes_and_ids):
     url_for_getting_stikers = 'https://suppliers-api.wildberries.ru/api/v2/orders/stickers/pdf'
     data_for_pdf = ''
     results_file = []
     for barcode in barcodes_and_ids.keys():
-        pdfs=['pdf/blank.pdf']
-        for id in barcodes_and_ids[barcode]:
+        pdfs = ['pdf/blank.pdf']
+        for id in barcodes_and_ids[barcode]['orders']:
             json_orders_id = {
                 "orderIds": [int(id)]
             }
@@ -142,22 +163,64 @@ def create_pdf_stickers_by_barcodes_and_ids(barcodes_and_ids):
     merger.write('results.pdf')
     merger.close()
 
-if __name__ == '__main__':
+
+def getting_information_about_barcode_by_chartId(chrtId):
+    url = 'https://suppliers-api.wildberries.ru/card/list'
+    json_for_request = {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "params": {
+            "filter": {
+                "find": [
+                    {
+                        "column": "nomenclatures.variations.chrtId",
+                        "search": chrtId
+                    }
+                ],
+                "order": {
+                    "column": "string",
+                    "order": "string"
+                }
+            }
+        }
+    }
+    response = requests.post(url=url, headers=headers, json=json_for_request)
+    good = response.json()['result']['cards'][0]
+    name = ''
+    article = good['nomenclatures'][0]['vendorCode']
+    # print(good.keys())
+    # print(good['nomenclatures'][0]['vendorCode'])
+    for type_and_params in good['addin']:
+        if type_and_params['type'] == 'Наименование':
+            name = type_and_params['params'][0]['value']
+    info = {
+        'name': name,
+        'article': article,
+        'chrtId': chrtId
+    }
+    return info
+
+
+def add_information_about_barcodes(barcodes):
+    for barcode in barcodes.keys():
+        barcodes[barcode]['info'] = getting_information_about_barcode_by_chartId(barcodes[barcode]['chrtId'])
+    return barcodes
+
+def create_stickers():
     orders = get_all_orders(status=1)
-    # orders.sort(key=barcode_key_for_sorting)
     orders = sorted(orders, key=lambda x: x['barcode'])
-    # print(orders[:10])
-    # for i in range(51):
-    #     print(orders[i]['barcode'])
-    orders_ids = get_orders_ids(orders)
-    print(len(orders))
-    barcodes_and_ids = get_barcodes_and_ids(orders)
-    print(barcodes_and_ids.keys())
-    # with open('tmp.txt', 'w') as f:
-    #     print(orders_ids, file=f)
-    with open('ids_and_barcode.txt', 'w') as f:
-        for order in orders:
-            print(order['orderId'], order['barcode'], file=f)
-    # create_pdf_stickers_by_ids(orders_ids)
-    create_pdf_stickers_by_barcodes_and_ids(barcodes_and_ids)
-    print(len(barcodes_and_ids.keys()))
+    barcodes = get_barcodes_with_orders_and_chartId(orders)
+    barcodes = add_information_about_barcodes(barcodes)
+    create_pdf_stickers_by_barcodes(barcodes)
+
+if __name__ == '__main__':
+    # canvas = Canvas('pdf/blank.pdf',pagesize=(113.4,  85.17))
+    # canvas.setFont('FreeSans', 4)
+    # canvas.drawString(2, 75, "Носки с принтом 'Новоssгодниеllll1adsfgsdgsgsdgsdgsdgds123", charSpace=0)
+    info = {
+        'name': "Носки с принтом 'Новоssssодние",
+        'article': 'WR0055/WR0055/03/36-41',
+        'chrtId': str(8156990)
+    }
+    edit_blank_pdf(info)
+    # canvas.save()   
