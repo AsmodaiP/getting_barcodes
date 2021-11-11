101881904f06c1e397c95d511254e2a52b36894e
Asmodai <asmoday.p@gmail.com>
2021-11-11 20:15:19 +0300

    Протестированная версия

--- create_stickers_and_db.py
+++ create_stickers_and_db.py
@@ -1,24 +1,21 @@
 import datetime
 from enum import IntFlag
 import json
-from math import e
-import re
-from sys import path
 import pytz
-from reportlab.lib.utils import char2int, prev_this_next
 import requests
 import codecs
 import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfFileMerger
-from requests.api import head
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.units import mm, inch
+from reportlab.lib.units import inch
 import logging
 from logging.handlers import RotatingFileHandler
 import openpyxl
+from requests.api import request
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 log_file = os.path.join(BASE_DIR, 'logs/bot.log')
@@ -37,19 +34,16 @@ logging.basicConfig(
         console_handler
     )
 )
-pdf_path = os.path.join(BASE_DIR, 'pdf/')
-if not os.path.exists(pdf_path):
-    os.mkdir(pdf_path)
-today_path = os.path.join(pdf_path, str(datetime.date.today()))
 
-
-if not os.path.exists(today_path):
-    os.mkdir(today_path)
 
 pdfmetrics.registerFont(TTFont('FreeSans', 'fonts/FreeSans.ttf'))
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+def create_path_if_not_exist(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
 def get_now_time():
@@ -60,10 +54,21 @@ def get_now_time():
 
 TOKEN = os.environ.get('TOKEN')
 base_url_for_getting_orders = 'https://suppliers-api.wildberries.ru/api/v2/orders'
-url = 'https://suppliers-api.wildberries.ru/api/v2/orders?date_start=2021-11-06T08%3A47%3A17.528082%2B00%3A00&date_end=2021-11-06T11%3A19%3A03.297640%2B00%3A00&status=2&take=2&skip=0'
 headers = {
     'Authorization': TOKEN,
 }
+
+
+cred = json.load(open('credentials.json', 'rb'))
+TOKEN = cred['БелотеловАГ']['token']
+NAME = cred['БелотеловАГ']['name']
+
+pdf_path = os.path.join(BASE_DIR, 'pdf/')
+create_path_if_not_exist(pdf_path)
+create_path_if_not_exist(os.path.join(pdf_path, str(datetime.date.today())))
+today_path_wiht_name = os.path.join(pdf_path, str(datetime.date.today()), NAME)
+create_path_if_not_exist(today_path_wiht_name)
+# results_path = os.path.join()
 
 
 def create_pdf_stickers_by_ids(ids):
@@ -80,7 +85,7 @@ def create_pdf_stickers_by_ids(ids):
             headers=headers)
         data_for_pdf = response.json()['data']['file']
         file_data = bytes(data_for_pdf, 'utf-8')
-        path = os.path.join(today_path, f'{id}.pdf')
+        path = os.path.join(today_path_wiht_name, f'{id}.pdf')
         with open(path, 'wb') as f:
             f.write(codecs.decode(file_data, 'base64'))
         pdfs += [path]
@@ -119,7 +124,8 @@ def get_all_orders(status=0, date_end=get_now_time()):
             params=params)
         orders_from_current_response = response.json()['orders']
         orders += orders_from_current_response
-    logging.info(f'Получено {len(orders)-1} заказов')
+        logging.info(f'+{len(orders_from_current_response)}')
+    logging.info(f'Получено {len(orders)}')
     return orders
 
 
@@ -150,33 +156,34 @@ def get_barcodes_with_orders_and_chartId(orders):
     return barcodes_and_ids
 
 
-def edit_blank_pdf(barkode_info):
+def edit_blank_pdf(barcode_info):
 
-    canvas = Canvas('pdf/blank1.pdf', pagesize=(1.6*inch,  1.2*inch))
+    canvas = Canvas('pdf/blank1.pdf', pagesize=(1.6 * inch, 1.2 * inch))
     font_size = 9
     canvas.setFont('FreeSans', font_size)
     n = 0
     blanks_number = 1
     slice = 23
-    name = barkode_info['name']
-    size = barkode_info['size']
-    article = f'article = {barkode_info["article"]}'
-    color = barkode_info['color']
-    extra_colors = barkode_info['extra_colors']
-    chrtId = f'chrtId = {barkode_info["chrtId"]}'
+    name = barcode_info['name']
+    size = barcode_info['size']
+    article = f'article = {barcode_info["article"]}'
+    color = barcode_info['color']
+    extra_colors = barcode_info['extra_colors']
+    count = 'Количество = ' + str(barcode_info['count'])
+    chrtId = f'chrtId = {barcode_info["chrtId"]}'
     blanks = ['pdf/blank1.pdf']
-    for params in [name, size, color, extra_colors, article, chrtId]:
+    for params in [count, name, size, color, extra_colors, article, chrtId]:
         a = str(params)
         while len(a) > 0:
             if n > 8:
                 blanks_number += 1
                 canvas.save()
                 canvas = Canvas(
-                    f'pdf/blank{blanks_number}.pdf', pagesize=(1.6*inch,  1.2*inch))
+                    f'pdf/blank{blanks_number}.pdf', pagesize=(1.6 * inch, 1.2 * inch))
                 canvas.setFont('FreeSans', font_size)
                 blanks += [f'pdf/blank{blanks_number}.pdf']
                 n = 0
-            canvas.drawString(2, 75-n*font_size,
+            canvas.drawString(2, 75 - n * font_size,
                               a[:slice], charSpace=0)
             a = a[slice:]
             n += 1
@@ -187,10 +194,13 @@ def edit_blank_pdf(barkode_info):
     merg.write('pdf/blank.pdf')
     merg.close()
     logging.info(
-        f'Получена информация: наименование "{barkode_info["name"]}" '
-        f'артикул = {article}, chrtId = {chrtId} '
-        f'{"Size "+size if size != "" else ""}{" color "+ color if color != "" else ""}'
-        f'{extra_colors if extra_colors != "" else ""}')
+        f'Получена информация: наименование "{barcode_info["name"]}" '
+        f' {count}, '
+        f'артикул = {article}, '
+        f'chrtId = {chrtId} '
+        f'{"Size "+size if size != "" else ""} '
+        f'{"color "+ color if color != "" else ""} '
+        f'{"Доп цвета: "+extra_colors if extra_colors != "" else ""} ')
 
 
 def get_StickerEncoded_by_orderId(id):
@@ -222,7 +232,7 @@ def create_and_merge_pdf_by_barcodes_and_ids(barcodes_and_ids):
                 headers=headers)
             data_for_pdf = response.json()['data']['file']
             file_data = bytes(data_for_pdf, 'utf-8')
-            path = os.path.join(today_path, f'{id}.pdf')
+            path = os.path.join(today_path_wiht_name, f'{id}.pdf')
             with open(path, 'wb') as f:
                 f.write(codecs.decode(file_data, 'base64'))
             pdfs += [path]
@@ -230,7 +240,7 @@ def create_and_merge_pdf_by_barcodes_and_ids(barcodes_and_ids):
         for pdf in pdfs:
             merger.append(pdf)
         path_for_result_of_barcode = os.path.join(
-            today_path, f'result_{barcode}.pdf')
+            today_path_wiht_name, f'result_{barcode}.pdf')
         merger.write(path_for_result_of_barcode)
         results_files.append(path_for_result_of_barcode)
         merger.close()
@@ -281,6 +291,7 @@ def get_card_by_chrtId(chrtId):
         }
     }
     response = requests.post(url=url, headers=headers, json=json_for_request)
+    # print(response.content)
     card = response.json()['result']['cards'][0]
     return card
 
@@ -320,10 +331,13 @@ def getting_information_about_barcode_by_chartId(chrtId):
     extra_colors = ''
     addin = nomenclature['addin']
     for type_and_params in addin:
+        # print(type_and_params['params'])
+        # print(type_and_params)
         if type_and_params['type'] in ('Цвет', 'Основной цвет'):
             color = type_and_params['params'][0]['value']
-        if type_and_params['type'] == 'Доп. цвет':
-            extra_colors = type_and_params['params'][0]['value']
+        if type_and_params['type'] == 'Доп. цвета':
+            for extra_color in type_and_params['params']:
+                extra_colors += extra_color['value']+' '
     if 'Доп. цвета' in nomenclature.keys():
         extra_colors = nomenclature['Доп. цвета']
     for type_and_params in good['addin']:
@@ -345,10 +359,11 @@ def getting_information_about_barcode_by_chartId(chrtId):
     return info
 
 
-def add_information_about_barcodes(barcodes):
+def add_information_about_barcodes_and_len(barcodes):
     for barcode in barcodes.keys():
         barcodes[barcode]['info'] = getting_information_about_barcode_by_chartId(
             barcodes[barcode]['chrtId'])
+        barcodes[barcode]['info']['count'] = len(barcodes[barcode]['orders'])
     return barcodes
 
 
@@ -357,34 +372,50 @@ def create_db_for_checking(barcodes):
     logging.info('Получение расшифрованных стикеров')
     for barcode in barcodes.keys():
         logging.info(f'Получение расшифрованных стикеров для {barcode}')
-        barcodes_and_stickers[barcode]={}
+        barcodes_and_stickers[barcode] = {}
         arcticle = barcodes[barcode]['info']['article']
+        name = barcodes[barcode]['info']['name']
+        size = barcodes[barcode]['info']['size']
+        color = barcodes[barcode]['info']['color']
         for order in barcodes[barcode]['orders']:
-            sticker_encoded = [get_StickerEncoded_by_orderId(order)]
-            barcodes_and_stickers[barcode][order]= {
+            sticker_encoded = get_StickerEncoded_by_orderId(order)
+            barcodes_and_stickers[barcode][order] = {
                 'sticker_encoded': sticker_encoded,
-                'article': arcticle
+                'article': arcticle,
+                'name': name[:40],
+                'size': size,
+                'color': color
             }
 
     book = openpyxl.Workbook()
     sheet = book.active
     sheet['A1'] = 'Номер заказа'
     sheet['B1'] = 'Артикул'
-    sheet['C1'] = 'Barcode'
-    sheet['D1'] = 'Sticker'
+    sheet['C1'] = 'Наименование'
+    sheet['D1'] = 'Баркод'
+    sheet['E1'] = 'Stick'
+    sheet['H1']= 'Баркод'
+    sheet['I1']= 'Количество'
+
     row = 2
+    row_for_counts = 2
     logging.info('Формирование xlsx файла')
     for barcode in barcodes_and_stickers.keys():
         barcode_info = barcodes_and_stickers[barcode]
+        sheet[row_for_counts][7].value = str(barcode)
+        sheet[row_for_counts][8].value = len(barcode_info.keys())
+        row_for_counts += 1
         for order in barcode_info.keys():
             info = barcode_info[order]
             article = info['article']
             sticker_encoded = info['sticker_encoded']
+            name = info['name']
             sheet[row][0].value = str(order)
             sheet[row][1].value = str(article)
-            sheet[row][2].value = str(barcode)
-            sheet[row][3].value = ''.join(sticker_encoded)
-            row += 1           
+            sheet[row][2].value = name
+            sheet[row][3].value = str(barcode)
+            sheet[row][4].value = sticker_encoded
+            row += 1
         # article = barcodes_and_stickers[barcode]['article']
         # for order in barcodes_and_stickers[barcode]['orders']:
         #     sheet[row][0].value - str(order)
@@ -396,27 +427,152 @@ def create_db_for_checking(barcodes):
     book.close()
 
 
+
+
 def check_and_delete_orders_with_blank_officeAddress(orders):
+    logging.info('Производится отчистка от заказов без адреса')
     for order in orders:
         if order['officeAddress'] == "":
-            logging.info(f'Удален заказ {order["orderId"]} без адреса')
+            logging.info(f'Удален заказ {order["orderId"]}')
             orders.remove(order)
+    logging.info(f'Осталось {len(orders)}')
     return orders
 
+def set_status_to_orders(status, orders):
+    url_for_set_status = 'https://suppliers-api.wildberries.ru/api/v2/orders'
+    data_for_bulk_set_status = []
+    for order in orders:
+        data_for_bulk_set_status += [{
+            "orderId": order["orderId"],
+            "status": int(status)
+        }]
+    for order in orders:
+        data_for_set_status = [{
+            "orderId": order["orderId"],
+            "status": int(status)
+        }]
+        js = json.dumps(data_for_set_status)
+        response = requests.put(url_for_set_status, headers=headers, data=js)
+        logging.info(response.content)
+
+# def get_all_orders_by_status_and_articul(status, articul,date_end=get_now_time()):
+#     logging.info(f'Получение заказов с статусом {status} и артикулом {articul}')
+#     date_start = '2021-11-06T00:47:17.528082+00:00'
+#     orders = []
+#     params = {
+#         'date_end': date_end,
+#         'date_start': date_start,
+#         'status': status,
+#         'take': 100,
+#         'skip': 0
+#     }
+#     response = requests.get(
+#         base_url_for_getting_orders,
+#         headers=headers,
+#         params=params)
+#     try:
+#         orders_from_current_response = response.json()['orders']
+#     except KeyError as e:
+#         logging.error(e, exc_info=True)
+#     orders += orders_from_current_response
+#     while orders_from_current_response != []:
+#         params['skip'] += len(orders_from_current_response)
+#         response = requests.get(
+#             base_url_for_getting_orders,
+#             headers=headers,
+#             params=params)
+#         orders_from_current_response = response.json()['orders']
+#         orders += orders_from_current_response
+#         logging.info(f'+{len(orders_from_current_response)}')
+#     logging.info(f'Получено {len(orders)}')
+#     return orders
 
 def create_stickers():
     orders = get_all_orders(status=1)
-    orders = check_and_delete_orders_with_blank_officeAddress(orders)
+    # orders = check_and_delete_orders_with_blank_officeAddress(orders)
     orders = sorted(orders, key=lambda x: x['barcode'])
     barcodes = get_barcodes_with_orders_and_chartId(orders)
-    barcodes = add_information_about_barcodes(barcodes)
+    barcodes = add_information_about_barcodes_and_len(barcodes)
     create_pdf_stickers_by_barcodes(barcodes)
     create_db_for_checking(barcodes)
 
+# def filter_orders_by_barcode(orders, barcode):
+#     for order in orders:
+#         if order['barcode'] != barcode:
+#             orders.del(order)
+
+def set_status_collected_for_all_on_assembly():
+    orders = get_all_orders(status=1)
+    set_status_to_orders(2, orders)
+    logging.info(f'{len(orders)} заказов переведены в собранные')
 
 if __name__ == '__main__':
+    # orders = get_all_orders(status=1)
+    # orders = filter_orders_by_barcode(orders, "2000790297008")
+    # print(orders)
     create_stickers()
-    # print(get_card_by_nmid(34778015))
-    # info = getting_information_about_barcode_by_chartId(87378583)
-    # print(info)
+    # set_status_collected_for_all_on_assembly()
+    # orders = get_all_orders(status=1)
+
+    # set_status_to_orders(2, orders)
+    # for order in orders:
+    #     print(order['orderId'])
+    # info = {
+    #     'name': 'тестовое имя',
+    #     'article': 'какой-то там артикул',
+    #     'chrtId': 'chrid',
+    #     'size': 'размер',
+    #     'color': 'цвет',
+    #     'extra_colors': 'экстра цвет',
+    #     'count': 33
+    # }
     # edit_blank_pdf(info)
+    # orders = get_all_orders(status=1)
+#     orders = [
+#     {
+#       "orderId": 13833711,
+#       "dateCreated": "2021-02-20T16:50:33.365+03:00",
+#       "wbWhId": 119408,
+#       "storeId": 658434,
+#       "pid": 0,
+#       "officeAddress": "г Ставрополь (Ставропольский край), Ленина 482/1",
+#       "OfficeLatitude": 45.038605,
+#       "OfficeLongitude": 41.905666,
+#       "deliveryAddress": "улица, дом, квартира",
+#       "deliveryAddressDetails": {
+#         "province": "Челябинская область",
+#         "area": "Челябинск",
+#         "city": "Челябинск",
+#         "street": "51-я улица Арабкира",
+#         "home": "10А",
+#         "flat": "42",
+#         "entrance": "3",
+#         "longitude": 44.519068,
+#         "latitude": 40.20192
+#       },
+#       "userInfo": {
+#         "userId": 123,
+#         "fio": "Иванов Иван Иванович",
+#         "phone": 79991112233
+#       },
+#       "chrtId": 11111111,
+#       "barcode": 6665956397512,
+#       "barcodes": [
+#         6665956397512
+#       ],
+#       "status": 2,
+#       "userStatus": 2,
+#       "rid": 100321840623,
+#       "totalPrice": 5600,
+#       "orderUID": "string",
+#       "deliveryType": 1
+#     }
+#   ]
+#     # orders = check_and_delete_orders_with_blank_officeAddress(orders)
+#     # orders = sorted(orders, key=lambda x: x['barcode'])
+#     barcodes = get_barcodes_with_orders_and_chartId(orders)
+#     barcodes = add_information_about_barcodes_and_len(barcodes)
+#     print(barcodes)
+    # get_card_by_chrtId('17290060')
+    # card = get_card_by_nmid(17290060)
+    # print(card)
